ee2d55a00f67cf65f82ec531a6c3fc4228d2bcf2
kamaumike <kamaumike2013@gmail.com>
2016-11-14 12:30:42 +0300

    Created fibonacci function

---
+++ Day-0/fibonacci/fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+	"""Returns a Fibonacci sequence from 0 to n"""
+
+	sequence = [0,1]
+
+	# Loop through the numbers 
+	for i in range(2,n):
+		# Append the numbers to the sequence list
+		sequence.append(sequence[i-1] + sequence[i-2])		
+	# Return the fibonacci sequence
+	return "Your Fibonacci numbers are: %s" % (sequence)

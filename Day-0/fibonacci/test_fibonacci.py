4b5854807116973f6cea86e95f45d21dce20b6e8
kamaumike <kamaumike2013@gmail.com>
2016-11-14 14:09:27 +0300

    Added test_if_input_is_zero

--- Day-0/fibonacci/test_fibonacci.py
+++ Day-0/fibonacci/test_fibonacci.py
@@ -4,6 +4,9 @@ import unittest
 class FibonacciTestCase(unittest.TestCase):
 	"""Test cases for fibonacci function"""
 	
+	def test_if_input_is_zero(self):
+		self.assertEqual(fibonacci(0), "Your Fibonacci numbers are: %s" % ([0,1]))
+
 	def test_if_output_is_correct(self):
 		self.assertEqual(fibonacci(5), "Your Fibonacci numbers are: %s" % ([0,1,1,2,3]))
 
